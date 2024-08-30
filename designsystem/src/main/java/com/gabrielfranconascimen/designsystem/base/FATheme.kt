83b6e6e6a15a6ede4eb33c9e9aaf653adddaa949
Gabriel Franco Nascimento <gabrielfranconascimen@gmail.com>
2024-08-30 10:36:21 -0300

    Create components (#2)

---
+++ designsystem/src/main/java/com/gabrielfranconascimen/designsystem/base/FATheme.kt
@@ -0,0 +1,75 @@
+package com.gabrielfranconascimen.designsystem.base
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun FATheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkFAColor else LightFAColor
+    val dimension = DefaultDimensions
+
+    ProvideDimens(dimension = dimension) {
+        ProvideColors(colors = colors) {
+            MaterialTheme(
+                colorScheme = debugColors(Color.Magenta),
+                typography = Typography(),
+                shapes = Shapes(),
+                content = content
+            )
+        }
+    }
+}
+
+object FATheme {
+    val dimens: Dimensions
+        @Composable
+        get() = LocalAppDimens.current
+
+    val colors: FAColors
+        @Composable
+        get() = LocalAppColors.current
+}
+
+val Dimens: Dimensions
+    @Composable
+    get() = FATheme.dimens
+
+val Colors: FAColors
+    @Composable
+    get() = FATheme.colors
+
+
+@Composable
+fun ProvideColors(
+    colors: FAColors,
+    content: @Composable () -> Unit
+) {
+    val colorPalette = remember { colors.copy() }
+    colorPalette.update(colors)
+    CompositionLocalProvider( LocalAppColors provides colorPalette, content)
+}
+
+@Composable
+fun ProvideDimens(
+    dimension: Dimensions,
+    content: @Composable () -> Unit
+) {
+    val dimensionSet = remember { dimension }
+    CompositionLocalProvider(LocalAppDimens provides dimensionSet, content)
+}
+
+private val LocalAppColors = staticCompositionLocalOf<FAColors> {
+    error("No FAColorScheme provided")
+}
+
+private val LocalAppDimens = staticCompositionLocalOf { DefaultDimensions }
---
+++ designsystem/src/main/java/com/gabrielfranconascimen/designsystem/components/cards/FACard.kt
@@ -0,0 +1,61 @@
+package com.gabrielfranconascimen.designsystem.components.cards
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import com.gabrielfranconascimen.designsystem.base.Colors
+import com.gabrielfranconascimen.designsystem.base.Dimens
+
+@Composable
+fun FACard(
+    modifier: Modifier = Modifier,
+    borderShape: Shape = RoundedCornerShape(Dimens.roundButton),
+    borderStroke: BorderStroke? = BorderStroke(
+        Dimens.borderStroke,
+        Colors.borderGray
+    ),
+    cardColors: CardColors = CardDefaults.cardColors(
+        containerColor = Colors.backgroundPrimary,
+        contentColor = Colors.text
+    ),
+    elevation: CardElevation = CardDefaults.cardElevation(),
+    contentPadding: PaddingValues = PaddingValues(Dimens.mediumPadding),
+    onClick: (() -> Unit)? = null,
+    content: @Composable () -> Unit
+) {
+    Card(
+        modifier = modifier
+            .clip(borderShape)
+            .clickable(
+                enabled = onClick != null,
+                indication = rememberRipple(radius = Dp.Infinity),
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = { onClick?.invoke() }
+            ),
+        shape = borderShape,
+        border = borderStroke,
+        elevation = elevation,
+        colors = cardColors
+    ) {
+        Box(
+            modifier = Modifier.padding(contentPadding)
+        ) {
+            content()
+        }
+    }
+}
